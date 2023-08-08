257800cfbb2adde08604606a25823e7a7938e466
ManasComp <OndrejMan1@gmail.com>
2023-08-08 14:04:44 +0200

    Removes hstack

--- DevAcademy/Scenes/Places/PlacesRow.swift
+++ DevAcademy/Scenes/Places/PlacesRow.swift
@@ -44,31 +44,28 @@ struct PlacesRow: View {
     var body: some View {
         NavigationLink(destination: PlaceDetail(feature: feature)) {
             HStack {
-                HStack {
-                    AsyncImage(url: feature.properties.obrId1) {
-                        image in
-                        image
-                            .resizable()
-                            .frame(width: 60, height: 60)
-                            .aspectRatio(contentMode: .fill)
-                            .clipShape(RoundedRectangle(cornerRadius: 8))
-                            .shadow(radius: 4)
-                    } placeholder : {
-                        ProgressView()
-                    }
-                    VStack(alignment : .leading) {
-                        Text(feature.properties.nazev)
-                            .font(.title)
-                            .fontWeight(.semibold)
-                            .lineLimit(1)
-                        Text(feature.properties.druh.rawValue)
-                            .foregroundColor(.secondary)
-                            .font(.subheadline)
-                    }
+                AsyncImage(url: feature.properties.obrId1) {
+                    image in
+                    image
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .aspectRatio(contentMode: .fill)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .shadow(radius: 4)
+                } placeholder : {
+                    ProgressView()
+                }
+                VStack(alignment : .leading) {
+                    Text(feature.properties.nazev)
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .lineLimit(1)
+                    Text(feature.properties.druh.rawValue)
+                        .foregroundColor(.secondary)
+                        .font(.subheadline)
                 }
             }
         }
-        
     }
 }
 
